bc48d3424434cd1df134e5cb8f78c106998b4c2e
chloechan382-pixel <chloechan382@gmail.com>
2026-05-16 03:55:25 -0400

    rtrying shorts again

--- data/projects.js
+++ data/projects.js
@@ -85,7 +85,7 @@ url3: "https://www.youtube.com/watch?v=6TIl-3xs-og",
     type: "youtube",
 format: "shorts",
 url:  "https://www.youtube.com/shorts/L5_gvoBYQ8c",                    // ← VIDEO TYPE
-    url2:  "hhttps://www.youtube.com/shorts/PhW4xruDqMc",
+    url2:  "https://www.youtube.com/shorts/PhW4xruDqMc",
     url3: "https://www.youtube.com/shorts/b0927naS_Ww",
     // ↑ REPLACE THIS with your real YouTube link
 
